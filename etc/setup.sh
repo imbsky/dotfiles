7c9a804884653fa8c67de65cdde61c9a1a472d87
BSKY <git@bsky.moe>
2019-09-21 22:57:55 +0900

    Update setup.sh

--- etc/setup.sh
+++ etc/setup.sh
@@ -12,9 +12,7 @@ function is_macos() {
   fi
 }
 
-function install_brew() {
-  echo "🚧 Installing brew and brew stuff..."
-
+function install_or_update_brew() {
   function brew_bundle() {
     echo "🚧 Installing brew stuff..."
     brew bundle install --file="$HOME/src/github.com/imbsky/dotfiles/etc/homebrew/Brewfile" >/dev/null || fail "Could not install brew stuff!"
@@ -32,10 +30,29 @@ function install_brew() {
   fi
 }
 
+function install_or_update_nanorc() {
+  function download_nanorc() {
+    echo "🚧 Downloading nanorc..."
+    curl --silent --location --output "$HOME/src/github.com/imbsky/dotfiles/nanorc" -C - "https://raw.githubusercontent.com/scopatz/nanorc/master/nanorc" || fail "Could not download nanorc!"
+  }
+
+  if [ -d "$HOME/.nano" ]; then
+    echo "🚧 Updating nanorc..."
+    cd "$HOME/.nano"
+    git pull >/dev/null || fail "Could not pull latest master branch!"
+    download_nanorc
+  else
+    echo "🚧 Cloning nanorc..."
+    git clone https://github.com/scopatz/nanorc ~/.nano >/dev/null || fail "Could not clone nanorc repository!"
+    download_nanorc
+  fi
+}
+
 function setup() {
   if is_macos; then
     echo -e "🏃 Setting up my \e[34mmacOS\e[m environment..."
-    install_brew
+    install_or_update_brew
+    install_or_update_nanorc
   else
     echo -e "🏃 Setting up my \e[33mLinux\e[m environment..."
     echo "🚧 WIP!"
