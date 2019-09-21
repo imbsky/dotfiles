7763cce696602c1dd0afb27a4b564c1cf6a5b48b
BSKY <git@bsky.moe>
2019-09-21 22:15:17 +0900

    Add setup script

---
+++ etc/setup.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+function warn() {
+  echo -e "\e[33mWarn:\e[m $* 😱"
+}
+
+function fail() {
+  echo -e "\e[31mFail:\e[m $* 😭"
+  exit 1
+}
+
+function is_macos() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    true
+  elif [[ "$OSTYPE" == "linux"* ]]; then
+    false
+  else
+    fail "Unsupported platform"
+  fi
+}
+
+function install_brew() {
+  echo "🚧 Installing brew and brew stuff..."
+
+  function brew_bundle() {
+    echo "🚧 Installing brew stuff..."
+    brew bundle install --file="$HOME/src/github.com/imbsky/dotfiles/etc/homebrew/Brewfile" >/dev/null || fail "Could not install brew stuff"
+  }
+
+  if is_macos; then
+    if [ -x "$(type -p brew)" ]; then
+      warn "Brew is already installed!"
+      brew_bundle
+    else
+      echo "🚧 Installing brew..."
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >/dev/null || fail "Could not install brew"
+      brew_bundle
+    fi
+  fi
+}
+
+function setup() {
+  if is_macos; then
+    echo -e "🏃 Setting up my \e[34mmacOS\e[m environment..."
+    install_brew
+  else
+    echo -e "🏃 Setting up my \e[33mLinux\e[m environment..."
+    echo "🚧 WIP!"
+  fi
+  echo "🏁 Done!"
+}
+
+setup
