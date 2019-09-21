5f33df4192cfe6e8532ee2b5b4a15122bf2aa305
BSKY <git@bsky.moe>
2019-09-21 23:34:18 +0900

    Update setup.sh

--- etc/setup.sh
+++ etc/setup.sh
@@ -15,7 +15,7 @@ function is_macos() {
 function install_or_update_brew() {
   function brew_bundle() {
     echo "🚧 Installing brew stuff..."
-    brew bundle install --file="$HOME/src/github.com/imbsky/dotfiles/etc/homebrew/Brewfile" >/dev/null || fail "Could not install brew stuff!"
+    brew bundle install --file="$HOME/src/github.com/imbsky/dotfiles/etc/homebrew/Brewfile" >/dev/null || fail "Could not install brew stuff"
   }
 
   if is_macos; then
@@ -24,26 +24,40 @@ function install_or_update_brew() {
       brew_bundle
     else
       echo "🚧 Installing brew..."
-      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >/dev/null || fail "Could not install brew!"
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >/dev/null || fail "Could not install brew"
       brew_bundle
     fi
+  fi
+}
+
+function install_or_update_fisher() {
+  if [ -d "$HOME/.config/fish/completions/fisher.fish" ]; then
+    echo "🚧 Downloading fisher..."
+    curl --silent --location --create-dirs --output "$HOME/.config/fish/functions/fisher.fish" "https://git.io/fisher" || fail "Could not download fisher"
+    echo "🚧 Installing fisher packages..."
+    fisher add matchai/spacefish >/dev/null || fail "Could not install 'matchai/spacefish'"
+    fisher add decors/fish-ghq >/dev/null || fail "Could not install 'decors/fish-ghq'"
+    fisher add oh-my-fish/plugin-peco >/dev/null || fail "Could not install 'oh-my-fish/plugin-peco'"
+  else
+    echo "🚧 Update all fisher packages"
+    fisher >/dev/null || fail "Could not update fisher packages!"
   fi
 }
 
 function install_or_update_nanorc() {
   function download_nanorc() {
     echo "🚧 Downloading nanorc..."
-    curl --silent --location --output "$HOME/src/github.com/imbsky/dotfiles/nanorc" -C - "https://raw.githubusercontent.com/scopatz/nanorc/master/nanorc" || fail "Could not download nanorc!"
+    curl --silent --location --create-dirs --output "$HOME/src/github.com/imbsky/dotfiles/nanorc" "https://raw.githubusercontent.com/scopatz/nanorc/master/nanorc" || fail "Could not download nanorc"
   }
 
   if [ -d "$HOME/.nano" ]; then
     echo "🚧 Updating nanorc..."
     cd "$HOME/.nano"
-    git pull >/dev/null || fail "Could not pull latest master branch!"
+    git pull >/dev/null || fail "Could not pull latest master branch"
     download_nanorc
   else
     echo "🚧 Cloning nanorc..."
-    git clone https://github.com/scopatz/nanorc ~/.nano >/dev/null || fail "Could not clone nanorc repository!"
+    git clone https://github.com/scopatz/nanorc $HOME/.nano >/dev/null || fail "Could not clone nanorc repository"
     download_nanorc
   fi
 }
@@ -52,6 +66,7 @@ function setup() {
   if is_macos; then
     echo -e "🏃 Setting up my \e[34mmacOS\e[m environment..."
     install_or_update_brew
+    install_or_update_fisher
     install_or_update_nanorc
   else
     echo -e "🏃 Setting up my \e[33mLinux\e[m environment..."
