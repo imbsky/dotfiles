9094acfb498a076621b6a675bde7fe964bf39028
BSKY <git@bsky.moe>
2019-11-25 03:44:07 +0900

    Update config.el

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,4 +1,13 @@
+(setq doom-font (font-spec :family "Fira Code" :size 14))
+
+(setq org-directory "~/src"
+      org-ellipsis " ▼ "
+      org-bullets-bullet-list '("#"))
+(after! org
+  (add-to-list 'org-modules 'org-habit t))
+
 (setq company-idle-delay 0
       company-show-numbers t)
 
 (def-package! company-tabnine :init (add-to-list 'company-backends 'company-tabnine))
+TabNine::config
