97f7a561f57bb57c1faa12deb5dc996b6ef18126
BSKY <git@bsky.moe>
2019-11-25 21:05:00 +0900

    Update config.el

--- doom.d/config.el
+++ doom.d/config.el
@@ -5,9 +5,13 @@
 (setq company-idle-delay 0
       company-show-numbers t)
 
-(def-package! company-tabnine :init (add-to-list 'company-backends 'company-tabnine))
+(setq treemacs-follow-mode t
+      treemacs-filewatch-mode t
+      treemacs-fringe-indicator-mode t)
 
 (eval-after-load 'web-mode
   '(progn
        (add-hook 'web-mode-hook 'add-node-modules-path)
        (add-hook 'web-mode-hook 'prettier-js-mode)))
+
+(def-package! company-tabnine :init (add-to-list 'company-backends 'company-tabnine))
