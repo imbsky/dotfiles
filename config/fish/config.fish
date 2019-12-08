62004a1ab9feb9b494c6acbbf316c39dd18b5400
BSKY <git@bsky.moe>
2019-12-09 05:14:52 +0900

    Update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,9 @@
 # Starship
 starship init fish | source
 
+# asdf version manager
+source ~/.asdf/asdf.fish
+
 # opam configuration
 source ~/.opam/opam-init/init.fish >/dev/null 2>/dev/null || true
 
