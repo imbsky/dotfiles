65ef4d2c30e8b93f43ceaed4d2309dc8cb65a272
BSKY <git@bsky.moe>
2019-12-18 02:35:07 +0900

    Update...

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,19 @@ starship init fish | source
 # asdf version manager
 source ~/.asdf/asdf.fish
 
+# Brew
+set -gx PATH /usr/local/sbin $PATH
+
+set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+set -gx PATH /usr/local/opt/ed/libexec/gnubin $PATH
+set -gx PATH /usr/local/opt/findutils/libexec/gnubin $PATH
+set -gx PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+set -gx PATH /usr/local/opt/gnu-tar/libexec/gnubin $PATH
+set -gx PATH /usr/local/opt/grep/libexec/gnubin $PATH
+
+# LLVM
+set -gx PATH /usr/local/opt/llvm/bin $PATH
+
 # opam configuration
 source ~/.opam/opam-init/init.fish >/dev/null 2>/dev/null || true
 
