5130d32592fb0c421aedbb7cce4a6478b522fe91
BSKY <git@bsky.moe>
2020-04-07 20:11:33 +0900

    Update for WSL2

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -1,14 +1,9 @@
-# gcc
-alias gcc 'gcc-9'
-alias g++ 'g++-9'
-
 # Easier navigation: .., ..., ...., ....., ~ and -
 alias ... 'cd ../..'
 alias .... 'cd ../../..'
 alias ..... 'cd ../../../..'
 
 # Shortcuts
-alias dl 'cd ~/Downloads'
 alias rf 'rm -rf'
 
 # yarn
@@ -18,40 +13,12 @@ alias yarn 'env CHILD_CONCURRENCY=(getconf _NPROCESSORS_ONLN) NETWORK_CONCURRENC
 alias bi 'bundle install --path vendor/bundle --jobs=(getconf _NPROCESSORS_ONLN)'
 alias be 'bundle exec'
 
-# homebrew
-alias brewup 'brew update && brew upgrade && brew cask upgrade && brew cleanup && brew doctor && brew cask doctor'
-
 # opam shortcut
 alias opamup 'opam update --all --upgrade --jobs=(getconf _NPROCESSORS_ONLN) --yes'
-
-# IP addresses
-alias pubip4 'dig +short -4 myip.opendns.com @resolver1.opendns.com'
-alias pubip6 'dig +short -6 myip.opendns.com aaaa @resolver1.ipv6-sandbox.opendns.com'
-alias localip 'ifconfig | grep -Eo "inet (addr:)?([0-9]*\\.){3}[0-9]*" | grep -Eo "([0-9]*\\.){3}[0-9]*" | grep -v "127.0.0.1"'
-alias ips 'ifconfig -a | grep -o "inet6\\? \\(addr:\\)\\?\\s\\?\\(\\(\\([0-9]\\+\\.\\)\\{3\\}[0-9]\\+\\)\\|[a-fA-F0-9:]\\+\\)" | awk "{ sub(/inet6? (addr:)? ?/, \"\"); print }"'
-
-# Show active network interfaces
-alias ifactive 'ifconfig | pcregrep -M -o "^[^\t:]+:([^\n]|\n\t)*status: active"'
-
-# Clean up LaunchServices to remove duplicates in the “Open With” menu
-alias lscleanup '/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder'
-
-# Copy working directory
-alias cwd 'pwd | tr -d "\r\n" | pbcopy'
-
-# macOS has no `md5sum`, so use `md5` as a fallback
-alias md5sum 'md5'
 
 # youtube-dl
 alias mp3dl 'youtube-dl -f mp3'
 alias mp4dl 'youtube-dl -f mp4'
 
-# Google Chrome
-alias chrome '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
-
-# Kill all the tabs in Chrome to free up memory
-# [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
-alias chromekill 'ps ux | grep "[C]hrome Helper --type=renderer" | grep -v extension-process | tr -s " " | cut -d " " -f2 | xargs kill'
-
-# Lock the screen (when going AFK)
-alias afk '/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
+# Copy working directory
+alias cwd 'wslpath -w (pwd) | clip.exe'
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,44 +1,5 @@
-# Starship
-# starship init fish | source
-
 # asdf version manager
 source ~/.asdf/asdf.fish
 
-# direnv
-direnv hook fish | source
-set -gx DIRENV_LOG_FORMAT
-
-# Yarn
-set -gx PATH ~/.yarn/bin $PATH
-
-# Brew
-set -gx HOMEBREW_MAKE_JOBS (getconf _NPROCESSORS_ONLN)
-
-set -gx PATH /usr/local/sbin $PATH
-
-set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-set -gx PATH /usr/local/opt/ed/libexec/gnubin $PATH
-set -gx PATH /usr/local/opt/findutils/libexec/gnubin $PATH
-set -gx PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
-set -gx PATH /usr/local/opt/gnu-tar/libexec/gnubin $PATH
-set -gx PATH /usr/local/opt/gnu-which/libexec/gnubin $PATH
-set -gx PATH /usr/local/opt/gnu-which/libexec/gnubin $PATH
-set -gx PATH /usr/local/opt/grep/libexec/gnubin $PATH
-set -gx PATH /usr/local/opt/util-linux/bin $PATH
-
-# LLVM
-set -gx PATH /usr/local/opt/llvm/bin $PATH
-
 # opam
-source ~/.opam/opam-init/init.fish >/dev/null 2>/dev/null || true
-
-# .NET
-set -gx PATH ~/.dotnet/tools $PATH
-
-# Go
-set -gx GOPATH ~/go
-set -gx PATH $GOPATH/bin $PATH
-set -gx GO111MODULE on
-
-# SDKROOT
-set -gx SDKROOT (env DEVELOPER_DIR=/Library/Developer/CommandLineTools xcrun --sdk macosx --show-sdk-path)
+source /home/konata/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
