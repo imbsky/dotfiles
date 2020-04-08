3f9b1feaaeb73f0a749bfd5a9432031f97cc080e
BSKY <git@bsky.moe>
2020-04-08 13:00:41 +0900

    Update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,9 @@
 # asdf version manager
 source ~/.asdf/asdf.fish
 
+# direnv
+direnv hook fish | source
+set -gx DIRENV_LOG_FORMAT
+
 # opam
 source /home/konata/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
