04030bc79dcf1f5f34504935e9c0a48aeba6622c
Sora Morimoto <sora@morimoto.io>
2020-11-14 01:50:48 +0900

    Update abbr.fish
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- config/fish/conf.d/abbr.fish
+++ config/fish/conf.d/abbr.fish
@@ -1,21 +1,16 @@
 # git
 abbr g 'git'
 abbr ga 'git add'
-abbr gaa 'git add . -A'
 abbr gc 'git commit -sm'
 abbr gcf 'git clean -df'
 abbr gic 'git commit --allow-empty -sm "Initial commit"'
-abbr gl 'git log'
-abbr gp 'git push'
 abbr gpmb 'git branch --merged | grep -v master | xargs -P (nproc) git branch -D'
-abbr gpo 'git pull origin'
 abbr gr 'git reset --hard'
 abbr grh 'git reset HEAD^1'
 abbr grpo 'git remote prune origin'
 abbr gs 'git switch'
 abbr gsc 'git switch -c'
 abbr gss 'git status -s'
-abbr gt 'git tag -l'
 
 # youtube-dl
 abbr mp3dl 'youtube-dl -f mp3'
